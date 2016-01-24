0053dc9c509b91a2e9c6d0ed7f3d3209ff289708
Yanis Vieilly <yanis.vieilly@gmail.com>
2016-01-25 00:22:23 +0100

    Add create action to questions controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,5 +4,18 @@ class QuestionsController < ApplicationController
   end
 
   def create
+    @question = Question.new(question_params)
+
+    if @question.save
+      render json: @question, status: :created
+    else
+      render json: @question.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title)
   end
 end

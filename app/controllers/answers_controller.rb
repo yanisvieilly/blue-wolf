c9ea5d5d4c97ce30fdff3e77400d7a154d610b40
Yanis Vieilly <yanis.vieilly@gmail.com>
2016-01-25 17:51:55 +0100

    Add controller code for answers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -3,5 +3,22 @@ class AnswersController < ApplicationController
   end
 
   def create
+    params['answers'].each do |question_id, answer|
+      question = Question.find(question_id)
+      answer = Answer.new(answer_params(answer))
+      if answer.save
+        question.answers << answer
+      else
+        render(json: answer.errors, status: :unprocessable_entity) && return
+      end
+    end
+
+    render json: true, status: :created
+  end
+
+  private
+
+  def answer_params(answer)
+    answer.permit(:value)
   end
 end

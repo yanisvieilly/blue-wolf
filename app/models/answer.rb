7c32d2495ec0497feb96838f69b92338c25afe81
Yanis Vieilly <yanis.vieilly@gmail.com>
2016-01-25 17:52:24 +0100

    Add validation to ensure the answer value is an integer between 0 and 10

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,9 @@
 class Answer < ActiveRecord::Base
   belongs_to :question
+
+  validates :value, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 0,
+    less_than_or_equal_to: 10
+  }
 end

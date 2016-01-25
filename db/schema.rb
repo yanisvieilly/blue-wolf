47ef235f3f17414b12911b3efbcc166bb9793533
Yanis Vieilly <yanis.vieilly@gmail.com>
2016-01-25 01:01:13 +0100

    Run pending migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160124223004) do
+ActiveRecord::Schema.define(version: 20160124235729) do
+
+  create_table "answers", force: :cascade do |t|
+    t.integer  "question_id"
+    t.integer  "value"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "answers", ["question_id"], name: "index_answers_on_question_id"
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"

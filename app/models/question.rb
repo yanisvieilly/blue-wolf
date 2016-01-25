70d134f8771ef0e752e8eb628cc6bb51595411b6
Yanis Vieilly <yanis.vieilly@gmail.com>
2016-01-25 23:55:08 +0100

    Ensure answers are deleted when their parent question is

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ActiveRecord::Base
-  has_many :answers, -> { order(created_at: :desc) }
+  has_many :answers, -> { order(created_at: :desc) }, dependent: :destroy
 
   validates :title, presence: true
 end

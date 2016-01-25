65f4b302c967e2244b2e5108f5aad508e628c4c5
Yanis Vieilly <yanis.vieilly@gmail.com>
2016-01-25 23:55:23 +0100

    Update Question model spec to check for dependent destroy parameter

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
 describe Question do
-  it { is_expected.to have_many(:answers).order(created_at: :desc) }
+  it do
+    is_expected
+      .to have_many(:answers)
+      .order(created_at: :desc)
+      .dependent(:destroy)
+  end
 
   it { is_expected.to validate_presence_of(:title) }
 end

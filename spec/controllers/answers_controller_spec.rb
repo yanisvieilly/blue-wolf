4f4f9e629e1ee7e8e4d2c7a9d52f1d5f69106467
Yanis Vieilly <yanis.vieilly@gmail.com>
2016-01-25 21:48:50 +0100

    Use shared spec in Answers controller spec

---
+++ spec/controllers/answers_controller_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+require 'shared_questions_index'
+
+describe AnswersController do
+  it_behaves_like 'a questions index'
+end
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,20 +1,8 @@
 require 'rails_helper'
+require 'shared_questions_index'
 
 describe QuestionsController do
-  describe 'GET #index' do
-    before { get :index }
-
-    it { is_expected.to respond_with :success }
-    it { is_expected.to render_template 'index' }
-
-    describe '@questions' do
-      let(:questions) { [Question.create(title: 'question title')] }
-
-      it 'matches the questions from the database' do
-        expect(assigns(:questions)).to match_array questions
-      end
-    end
-  end
+  it_behaves_like 'a questions index'
 
   describe 'GET #new' do
     before { get :new }

df565bff968fa4f3137ccb1d718f05090bedd65f
Yanis Vieilly <yanis.vieilly@gmail.com>
2016-01-25 22:05:25 +0100

    Add spec for answers#create when params are valid

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -6,5 +6,33 @@ describe AnswersController do
 
   describe 'POST #create' do
     it { is_expected.to use_before_action(:check_answers) }
+
+    context 'when parameters are valid' do
+      let(:questions) do
+        [
+          Question.create(title: 'question title'),
+          Question.create(title: 'another question title')
+        ]
+      end
+      let(:valid_params) do
+        {
+          answers: {
+            "#{questions.first.id}" => { value: '4' },
+            "#{questions.last.id}" => { value: '9' }
+          }
+        }
+      end
+
+      describe 'response' do
+        before { post :create, valid_params }
+
+        it { is_expected.to respond_with :created }
+      end
+
+      it 'creates one or more answers' do
+        expect { post :create, valid_params }
+          .to change { Answer.count }.by 2
+      end
+    end
   end
 end

091d2d72719ba4b2c3b6ee1d61bb785611b111d8
Yanis Vieilly <yanis.vieilly@gmail.com>
2016-01-25 21:52:10 +0100

    Check that the before_action is used

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -3,4 +3,8 @@ require 'shared_questions_index'
 
 describe AnswersController do
   it_behaves_like 'a questions index'
+
+  describe 'POST #create' do
+    it { is_expected.to use_before_action(:check_answers) }
+  end
 end

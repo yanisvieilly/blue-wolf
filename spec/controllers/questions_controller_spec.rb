40a95f17530b2c1c2f04065fb5f265d97918082a
Yanis Vieilly <yanis.vieilly@gmail.com>
2016-01-25 20:15:20 +0100

    Clean up questions#index spec

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -2,16 +2,14 @@ require 'rails_helper'
 
 describe QuestionsController do
   describe 'GET #index' do
-    it 'renders the index template' do
-      get :index
-      expect(response).to render_template 'index'
-    end
+    before { get :index }
+
+    it { is_expected.to render_template 'index' }
 
     describe '@questions' do
       let(:questions) { [Question.create(title: 'question title')] }
 
       it 'matches the questions from the database' do
-        get :index
         expect(assigns(:questions)).to match_array questions
       end
     end
